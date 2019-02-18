05642667346f01be73527fbeedd77d77a4f724a4
Wojciech Kalinski <wojciechmkalinski@gmail.com>
2019-02-18 22:14:43 +0100

    Added some learning for chest pain

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ import os
 df = pd.read_csv('heart.csv')
 
 df.sample(3)
+
+temp_chest_pain = (df.groupby(['target']))['cp'].value_counts(normalize=True)\
+.mul(100).reset_index(name = "percentage")
+sbn.barplot(x = "target", y = "percentage", hue = "cp", data = temp_chest_pain)\
+.set_title("Chest Pain vs Heart Disease")
+
+temp_thal = (df.groupby(['target']))['thal'].value_counts(normalize=True)\
+.mul(100).reset_index(name = "percentage")
+sbn.barplot(x = "target", y = "percentage", hue = "thal", data = temp_thal)\
+.set_title("thal vs Heart Disease")
+
+"""
+check attributes
+"""
+df.info()
+
+sbn.heatmap(df.corr())
