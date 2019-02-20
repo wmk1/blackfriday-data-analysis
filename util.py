bd4c54f647f8aadc6e6a536b1e2851df6f8276a3
Wojciech Kalinski <wojciechmkalinski@gmail.com>
2019-02-20 23:21:43 +0100

    Added util function that will translate all plots

---
+++ util.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class DataTranslators(Enum):
+    trestbps = "Resting blood pressure"
+    chol = "Serum cholesteral"
+    thalach = "Maximum heart rate achieved"
+    fbs = "Fasting blood sugar"
+    cp = "chest pain"
+    exang = "Excercise induced angina"
+    oldpeak = "ST Depression"
+    slope = "Slope of the peak"
+    ca = "Number of major vessels"
