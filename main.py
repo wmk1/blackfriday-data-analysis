fe62c0eba55f031ab6862b1208d10c4afdb98670
kkrystian <christian.kosciukiewicz@gmail.com>
2019-02-20 20:11:57 +0100

    ostatnie zmiany z tabelami - compilowalne

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plot
-import seaborn as sbn
-
-import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('heart.csv')
-
-df.sample(3)
-
-temp_chest_pain = (df.groupby(['target']))['cp'].value_counts(normalize=True)\
-.mul(100).reset_index(name = "percentage")
-sbn.barplot(x = "target", y = "percentage", hue = "cp", data = temp_chest_pain)\
-.set_title("Chest Pain vs Heart Disease")
-
-temp_thal = (df.groupby(['target']))['thal'].value_counts(normalize=True)\
-.mul(100).reset_index(name = "percentage")
-sbn.barplot(x = "target", y = "percentage", hue = "thal", data = temp_thal)\
-.set_title("thal vs Heart Disease")
-
-"""
-check attributes
-"""
+df.describe()
 df.info()
 
-sbn.heatmap(df.corr())
+sns.countplot(df.target,hue=df.sex, palette="Set3")
+plt.title("Count of Diseases and not")
+plt.xlabel("target")
+plt.ylabel("Count")
+plt.show()
+
+conditions=[(df.chol<200),
+            (df.chol<239),
+            (df.chol<1000)]
+choices=['low','natural','High']
+
+df.chol=np.select(conditions, choices)
+df.chol.tail(5)
+
+sns.stripplot(x="chol", y="age",order=["low","natural","High"],data=df)
+plt.ylabel("age")
+plt.xlabel("levels of cholesterol")
+plt.title('relation between age and type of cholesterol')
+plt.show()
+#with hue seperate famales=0 and males=1
+sns.stripplot(x="age",y="chol",data=df,order=["High","natural","low"],hue='sex')
+plt.ylabel('levels of cholesterol')
+plt.xlabel("age")
+plt.title("relation between age and type of chol by gender")
+plt.show()
